e521168a46058f59928ce73a3af11bfa740855e2
arirismansyah <16.9023@stis.ac.id>
2022-08-19 14:18:13 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\DsbsController;
+use App\Http\Controllers\API\DsrtController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +20,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login_android', [AuthController::class, 'login_android']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::POST('/get_dsbs_pcl', [DsbsController::class, 'get_dsbs_pcl']);
+    Route::POST('/get_dsbs_pml', [DsbsController::class, 'get_dsbs_pml']);
+    Route::POST('/get_dsrt_pcl', [DsrtController::class, 'get_dsrt_pcl']);
+    Route::POST('/get_dsrt_pml', [DsrtController::class, 'get_dsrt_pml']);
+
 });
